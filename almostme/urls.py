40a27516e3e36c057d04e0bcc6d67c4fd2f00aa5
Palkeeta30 <spalkeeta24@gmail.com>
2025-10-03 16:32:18 +0530

    Blackboxai/push almostme urls modified (#15)
    * Update README title without prefix
    
    * Update README title without prefix
    
    * Complete Railway deployment fixes: clean requirements, React build, SPA serving, env configs
    
    * Fix Railway deployment: clean requirements.txt encoding, update Procfile gunicorn command, add pip install to build
    
    * Add collected static files
    
    * Deploy: latest code for Railway deployment
    
    * Update README title without prefix
    
    * Update README title without prefix
    
    * Complete Railway deployment fixes: clean requirements, React build, SPA serving, env configs
    
    * Resolve merge and fix CORS/API for production
    
    * Add collected static files
    
    * Deploy: latest code for Railway deployment
    
    * Fix Procfile for Railway deployment
    
    * Update settings.py for Railway deployment
    
    * Fix Procfile: proper Gunicorn command for Railway
    
    * Fix signup API request failure: Use relative URLs in production and secure cookies
    
    * Fix signup API request failure: Use relative URLs in production and secure cookies
    
    * Fix signup API, Fitness page crash, and update staticfiles for React build
    
    * Fix deployment: copy React build to staticfiles for SPA serving on Railway
    
    * Update files from git status
    
    * Fix catch-all route path in urls.py to serve React index.html correctly
    
    * Add whitenoise staticfiles storage for production static files serving
    
    * Set DEBUG to False for production deployment
    
    * Modified almostme/urls.py

--- almostme/urls.py
+++ almostme/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
     path('games/', include('games.urls')),
     path('diet/', include('diet.urls')),
     path('fitness/', include('fitness.urls')),
